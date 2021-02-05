40d31444201b198210ae60e2c6c74833a1fbd080
tejal1497 <55185176+tejal1497@users.noreply.github.com>
2021-02-05 17:28:16 +0530

    Add files via upload

---
+++ mymodule.js
@@ -0,0 +1,11 @@
+var pi = 3.14;
+
+exports.numbers = [23,45,32,78];
+
+exports.msg = "welcome";
+
+exports.person = { "fname": "Bakul","lname" :"Joshi"};
+
+exports.calArea = function(r){
+   return pi*r*r;
+};

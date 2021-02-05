f38405eea982b7e2633ff3c528ea7867809f9dd3
tejal1497 <55185176+tejal1497@users.noreply.github.com>
2021-02-05 17:27:55 +0530

    Add files via upload

---
+++ usingmodule.js
@@ -0,0 +1,15 @@
+var mym = require('./mymodule.js');    //name of the module/path of the module - import
+
+console.log(mym.numbers[1]);
+
+console.log(mym.msg);
+
+console.log(mym.person.fname);
+
+console.log(mym.calArea(4.5));
+
+
+
+
+
+
